41c9cfe785ae910db2d0a97ce73b0a550f4e1900
Guilherme Vallim <guivallimaraujo@gmail.com>
2024-06-01 17:08:43 -0300

    commit

--- CrudExemplos/MallDelivery/app_mall/urls.py
+++ CrudExemplos/MallDelivery/app_mall/urls.py
@@ -6,5 +6,8 @@ from app_mall import views
 
 urlpatterns = [ 
     path('cadastro_lojista/', views.LojistaCreateView.as_view(), name='cadastro_lojista'),
-    path("", views.ProdutoList.as_view(), name="index"), 
+    path('cadastro_loja/', views.LojaCreateView.as_view(), name='cadastro_loja'),
+    path('add_produto/', views.ProdutoCreateView.as_view(), name='add_produto'),
+    path('produto/<int:pk>/deletar/', views.ProdutoDeleteView.as_view(), name='deletar_produto'),
+    path('', views.ProdutoList.as_view(), name='index'), 
 ] 
--- CrudExemplos/MallDelivery/app_mall/views.py
+++ CrudExemplos/MallDelivery/app_mall/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView
 
-from .models import  Produto, Lojista
+from .models import  Produto, Lojista, Loja
 from django.urls import reverse_lazy
  
 class ProdutoList(ListView): 
 
     model = Produto 
-
     template_name = "app_mall/index.html" 
+    context_object_name = 'produtos'
 
 
 
@@ -21,3 +21,23 @@ class LojistaCreateView(CreateView):
     fields = ['nome', 'CPF', 'telefone', 'email']
     template_name = 'cadastro_lojista.html'
     success_url = reverse_lazy('cadastro_lojista')
+
+
+class LojaCreateView(CreateView):
+    model = Loja
+    fields = ['nome', 'cnpj', 'descricao']
+    template_name = 'cadastro_loja.html'
+    success_url = reverse_lazy('cadastro_loja')
+
+class ProdutoCreateView(CreateView):
+    model = Produto
+    fields = ['nome', 'preco', 'descricao', 'estoque', 'categoria', 'loja']
+    template_name = 'add_produto.html'
+    success_url = reverse_lazy('add_produto')
+
+class ProdutoDeleteView(DeleteView): 
+
+    model = Produto
+    template_name = 'produto_confirm_delete.html'
+    success_url = reverse_lazy('index')
+
